8ac14927831c274ae9eb344621092fac1f1317e5
Andrea Cremonesi <andrea.cremonesi3@studenti.unimi.it>
2016-12-06 20:26:37 +0100

    modify insert query in registration file

--- registration.php
+++ registration.php
@@ -9,23 +9,21 @@
 	$response=array();
 
 	if( isset($_GET['user']) && strlen($_GET['user']) ){
-				
+		
 		$query="SELECT username FROM users WHERE username='".$_GET['user']."'";
 		$result=mysqli_query($conn,$query);
 
 		$row_number=mysqli_num_rows($result);
-		mysqli_close($conn);
 
 		// Non presente
 		if($row_number==0){
 
-			$conn = mysqli_connect($servername, $username, $password,$database); 
 			$query="INSERT INTO users VALUES ('".$_GET['user']."')";
 
-			$result=mysqli_query($conn,$query);
+			$result_insert=mysqli_query($conn,$query);
 
 			// Query riuscita
-			if($result){
+			if($result_insert){
 				$response["message"]="Perfect! Your username is ".$_GET['user']."";
 				$response["success"]=1;
 			}

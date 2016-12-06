70f58d710bcb5ac8f8f6989b0f7f8f2070dad4ab
Andrea Cremonesi <andrea.cremonesi3@studenti.unimi.it>
2016-12-06 21:20:43 +0100

    add script for recognize user

--- checkUser.php
+++ checkUser.php
@@ -9,7 +9,25 @@
 	$response=array();
 
 	if( isset($_GET['user']) && strlen($_GET['user'])){
+		$query="SELECT username FROM users WHERE username='".$_GET['user']."'";
+		$result=mysqli_query($conn,$query);
+
+		$rows=mysqli_num_rows($result);
+		
+		if($rows==0){
+			$response["message"]=$_GET['user']." is not existing";
+			$response["success"]=0;
+		}
+		else{
+			$response["message"]="Welcome back ".$_GET['user'];
+			$response["success"]=1;
+		}	
+
+	}
+	else{	
+		$response["message"]="Name is not inserted or name's length is over 20 characters";
+		$response["success"]=0;
 	}
 
-
+	echo json_encode($response);
 ?>

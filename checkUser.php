0ddbddb7226d9c81efb177d86d46a1488d404ba6
Andrea Cremonesi <andrea.cremonesi3@studenti.unimi.it>
2016-12-06 21:12:51 +0100

    add script for recognize user

---
+++ checkUser.php
@@ -0,0 +1,15 @@
+<?php
+	$servername = "127.2.50.2";
+	$username = "adminpHgZRmf";
+	$database = "streetskateology";
+	$password = "bnGlNf9YxIst";
+
+	$conn = mysqli_connect($servername,$username,$password,$database); 
+	
+	$response=array();
+
+	if( isset($_GET['user']) && strlen($_GET['user'])){
+	}
+
+
+?>

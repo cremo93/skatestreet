5ecfbd363df438981e118305849e80c6a733b79c
Andrea Cremonesi <andrea.cremonesi3@studenti.unimi.it>
2016-12-08 17:52:42 +0100

    script to find valutation of the user xxx

---
+++ checkLikeHate.php
@@ -0,0 +1,43 @@
+<?php
+	$servername = "127.2.50.2";
+	$username = "adminpHgZRmf";
+	$database = "streetskateology";
+	$password = "bnGlNf9YxIst";
+
+	
+
+	$conn = mysqli_connect($servername,$username,$password,$database); 
+	
+	$response=array();
+	
+	if(isset($_GET['user']){
+		$query="SELECT COUNT(*) AS tot,trick,hateLike FROM likes WHERE user='".$_GET['user']."'";
+		$result=mysqli_query($conn,$query);
+
+		if($result){
+			$pos=0;
+			
+			while($row=mysqli_fetch_array($result){
+				if($pos==0)
+					$response["count"]==$row['tot'];
+
+				$response["trick".$pos]=$row['trick'];
+				$response["hol".$pos]=$row['hateLike'];			
+				$pos++;	
+			}
+			if($pos==0)
+				// setterai ogni elemento come clickable
+				$response["hol".$pos]=2;
+			$response["success"]=1;
+			$response["message"]="Your hate or like!"
+		}
+		else{
+			$response["success"]=0;
+			$response["message"]="Wrong query!";
+		}
+		$response["success"]=0;
+		$response["message"]="Missing user!";
+	}
+
+
+?>

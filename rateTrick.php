281a274005cc03620d1a99301cc2d1e181339e1e
Andrea Cremonesi <andrea.cremonesi3@studenti.unimi.it>
2016-12-09 13:45:41 +0100

    added script to modify already rated trick!

--- rateTrick.php
+++ rateTrick.php
@@ -9,33 +9,71 @@
 	$response=array();
 	
 	if (isset($_GET['user']) && isset($_GET['tricks']) && isset($_GET['rate'])){
+		$_GET['tricks']=str_replace("_"," ",$_GET['tricks']);
 
-		
-		$query="INSERT INTO rates VALUES ('".$_GET['user']."','".$_GET['tricks']."',".$_GET['rate'].")";
+		$query="SELECT COUNT(*) as TOT,rate FROM rates WHERE user='".$_GET['user']."' AND tricks='".$_GET['tricks']."'";
 		$result=mysqli_query($conn,$query);
-
 		if($result){
+			$tot=mysqli_fetch_array($result);
+			$tot=$row['TOT'];
 
-			
-			$query="UPDATE tricks SET rating=(rating + ".$_GET['rate'].")/(rating_counter+1), rating_counter=rating_counter+1 WHERE name='".$_GET['tricks']."'";				
-			
-
-			$result=mysqli_query($conn,$query);
-		
-			if($result){
-				$response["message"]="Correct!";
-				$response["success"]=1;
+			if($tot>0){
+				$old_rate=$row['rate'];
+				$query="UPDATE rates SET rate=".$_GET['rate']." WHERE user='".$_GET['user']."' AND tricks='".$_GET['tricks']."'";
+				$result=mysqli_query($conn,$query);
+				if($result){
+					$toSum=$_GET['rate']-$old_rate;
+					$query="UPDATE tricks SET rate=(rate+".$toSum.")/(rating_counter) WHERE name='".$_GET['tricks']."'";
+					
+					$result=mysqli_query($conn,$query);
+					
+					if($result){
+						$response["success"]=1;
+						$response["message"]="Update your rate!";
+					}
+					else{
+						$response["success"]=0;
+						$response["message"]="Update failed!";
+					}
+				}
+				else{
+					$response["success"]=0;
+					$response["message"]="Failed trick update";
+				}
 			}
 			else{
-				$response["message"]="Oops! Update error!";
-				$response["success"]=0;
+				$query="INSERT INTO rates VALUES ('".$_GET['user']."','".$_GET['tricks']."',".$_GET['rate'].")";
+				$result=mysqli_query($conn,$query);
+
+				if($result){
+
+			
+					$query="UPDATE tricks SET rating=(rating + ".$_GET['rate'].")/(rating_counter+1), rating_counter=rating_counter+1 WHERE name='".$_GET['tricks']."'";				
+			
+
+					$result=mysqli_query($conn,$query);
+		
+					if($result){
+						$response["message"]="Correct!";
+						$response["success"]=1;
+					}
+					else{
+						$response["message"]="Oops! Update error!";
+						$response["success"]=0;
+					}
+				}
+				else{
+					$response["message"]="Oops! Insert Error!";
+					$response["success"]=0;
+				}
 			}
 		}
 		else{
-			$response["message"]="Oops! Insert Error!";
 			$response["success"]=0;
-			
+			$response["message"]="Oops! Count error!";
 		}
+		
+		
 
 	}
 	else{

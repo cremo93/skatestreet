733720f1b6cb67cd681571f3a2a6fb21e0177e02
Andrea Cremonesi <andrea.cremonesi3@studenti.unimi.it>
2016-12-10 20:29:05 +0100

    script to rank tricks

---
+++ rankTricks.php
@@ -0,0 +1,54 @@
+<?php
+	$servername = "127.2.50.2";
+	$username = "adminpHgZRmf";
+	$database = "streetskateology";
+	$password = "bnGlNf9YxIst";
+
+	$conn = mysqli_connect($servername,$username,$password,$database); 
+	
+	$response=array();
+
+	if (isset($_GET['rankby'])){
+		$query="";
+		switch($_GET['rankby']){
+			//five most liked
+			case 0:
+				$query="SELECT * FROM tricks ORDER BY likeSum DESC LIMIT 0,5";
+				
+			break;
+			
+			case 1:
+				$query="SELECT * FROM tricks ORDER BY dislike DESC LIMIT 0,5";
+			break;
+			
+			case 2:
+				$query="SELECT * FROM tricks ORDER BY rating DESC LIMIT 0,5";
+			break;
+		}
+		$result=mysqli_query($conn,$query);
+			if($result){
+						
+				for($pos=0;$pos<5;$pos++){
+					$response["name".$pos]=$row['name'];
+					$response["image_url".$pos]=$row['image_url'];
+					$response["video_id".$pos]=$row['video_id'];
+					$response["rating".$pos]=$row['rating'];
+					$response["like".$pos]=$row['likeSum'];
+					$response["dislike".$pos]=$row['dislike'];
+					$response["rating_counter".$pos]=$row['rating_counter'];
+					}
+				$response["success"]=1;
+				$response["message"]="Rank!";
+			}
+			else{
+				$response["success"]=0;
+				$response["message"]="Select error!";
+			}
+
+	}else{
+		$response["success"]=0;
+		$response["message"]="No parameters!";	
+	}
+
+	echo json_encode($response);		
+?>

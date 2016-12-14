04b5a1c22032f28a55f40ce19b3e11f39c80f0ab
Andrea Cremonesi <andrea.cremonesi3@studenti.unimi.it>
2016-12-14 12:30:56 +0100

    bug fixed

--- checkLikeHate.php
+++ checkLikeHate.php
@@ -11,28 +11,28 @@
 	$response=array();
 	
 	if(isset($_GET['user'])){
-		$query="SELECT COUNT(*) AS tot,trick,hateLike FROM likes WHERE user='".$_GET['user']."'";
+		$query="SELECT trick,hateLike FROM likes WHERE user='".$_GET['user']."'";
 		$result=mysqli_query($conn,$query);
 
 		// query non corretta
 		if($result){
 			$pos=0;
-			$response["count"]=$row['tot'];
+			
 			//scandisci
 			while($row=mysqli_fetch_array($result)){
-				if($pos==0) {$response["count"]=$row['tot'];}
+				
 					
 
 				$response["trick".$pos]=$row['trick'];
 
 				$response["hol".$pos]=$row['hateLike'];	
-				$pos++;	
+				$pos++;
 			}//chiudo while
-
+			
 			// setterai ogni elemento come clickable
 			if($pos==0) {$response["hol".$pos]=2;}
 				
-			//$response["count"]=$tot;
+			$response["count"]=$pos;
 			$response["success"]=1;
 			$response["message"]="Your hate or like!";
 		} // chiudo result
